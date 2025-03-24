bc03dcbec9ff093655a13a8fdbeb02f8ac088519
Краснов Никита <156430006+nikitakrasnov7@users.noreply.github.com>
2025-03-24 10:42:50 +0500

    Initial commit

---
+++ Assets/Skripts/Space State/SpaceXState.cs
@@ -0,0 +1,12 @@
+
+
+public abstract class SpaceXState
+{
+
+    public abstract void Upgrade();
+    public abstract void Repair();
+    public abstract void Salvage();
+
+    public abstract void Attack();
+
+}
